c9d7df7a39d71a04df863f6addb455f3330451c8
brettman <brettman@gmail.com>
2022-01-08 15:15:04 +0200

    changed it to return a string slice; notice return type of the first_word function is &str;  This indicates a string slice, which is different from a String;

--- src/main.rs
+++ src/main.rs
@@ -5,13 +5,15 @@ fn main() {
 }
 
 // return the index of the end of the first word
-fn first_word(s: &String) -> usize{
+fn first_word(s: &String) -> &str{
     let bytes = s.as_bytes();
     
+    // iter returns each element of array in a collection
+    // enumerate() wraps each element as a tuple (index, value)
     for(i, &item) in bytes.iter().enumerate(){
         if item == b' ' {
-            return i;
+           return &s[0..i];
         }
-    }
-    s.len()
+    };
+    &s[..]
 }
