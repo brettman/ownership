31d9d6f77f7711ef8efcbd033235c9feeb60f32f
brettman <brettman@gmail.com>
2022-01-08 14:04:30 +0200

    moving a pointer value;  this will not complie in rust; once s1 is moved to s2, s1 is no longer valid or accessible;

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 fn main() {
-    let mut s = String::from("hello");
-    s.push_str(", world!"); // push_str appends a literal to a string
-
-    println!("{}", s)
+    let s1 = String::from("hello");
+    let s2 = s1;
+    println!("{}", s1);
 }
