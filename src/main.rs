5e9cec7595b45cb0367f094e827df7e37e167e92
brettman <brettman@gmail.com>
2022-01-08 15:01:25 +0200

    starting to look at slices;

--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,17 @@
 fn main() {
-    example_one_tuples();
-    example_two_reference();
-    example_three_mutable_references();
+    let s = String::from("hello world");
+    let idx = first_word(&s);
+    println!("{}", idx);
 }
 
-fn example_one_tuples(){
-
-    let s1 = String::from ("hello");
-    let (s2, len) = calculate_length(s1);
-    // can't use s1, because it goes out of scope when we move it to
-    //  the function to calc length. 
-    // That function has to return a tuple with the original 
-    //  value and the length, to do the following:
-    println!("the length of '{}' is {}.", s2, len);
-}
-
-fn example_two_reference(){
-    let s1 = String::from("hello, again");
-    let len = calculate_length1(&s1);
-    println!("the length of '{}' is {}.", s1, len);
-}
-
-fn example_three_mutable_references(){
-    let mut s = String::from("hello");
-    change(&mut s);
-    println!("{}", s);
-}
-
-fn change(s: &mut String){
-    s.push_str(", world!");
-}
-
-fn calculate_length(s: String) -> (String, usize){
-    let l = s.len();
-
-    (s, l)      // notice lack of semi-colon? this means it is the return statement; 
-}
-
-fn calculate_length1(s: &String) -> usize{
+// return the index of the end of the first word
+fn first_word(s: &String) -> usize{
+    let bytes = s.as_bytes();
+    
+    for(i, &item) in bytes.iter().enumerate(){
+        if item == b' ' {
+            return i;
+        }
+    }
     s.len()
 }
