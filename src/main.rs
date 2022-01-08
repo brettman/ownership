66e3c82d5bb90bfd0777a603eb9c984c91010daf
brettman <brettman@gmail.com>
2022-01-08 14:27:52 +0200

    some basic code showing diff between move and copy with stack and heap variables;  comments in the code explain what is going on;

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,16 @@
 fn main() {
-    let s1 = String::from("hello");
-    let s2 = s1.clone();
-    println!("s1 = {}, s2 = {}", s1, s2);
+    let s = String::from("hello");  // s comes into scope
+    takes_ownership(s); // s moves its value to the function and is no longer valid
+
+    let x = 5;      // x comes into scope
+    makes_copy(x);  // i32 is `copy` so it's still ok to use x after this call
+
 }
+
+fn takes_ownership(some_string: String){  // some_string comes into scope
+    println!("{}", some_string); 
+} // Here, some_string goes out of scope and `drop` is called.  The backing memory is freed
+
+fn makes_copy(i: i32){  // i comes into scope
+    println!("{}", i);
+} // i goes out of scope, nothing special happens as this is a stack value
